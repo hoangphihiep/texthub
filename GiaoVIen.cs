8069afabfebfa2627d83270c1564a5041199ab43
Khoa432004 <khoavt123456789@gmail.com>
2024-01-23 10:27:35 +0700

    Them giao vien

---
+++ GiaoVIen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thuchanhgit
+{
+    public class GiaoVIen
+    {
+        string ten;
+        public GiaoVIen(string ten)
+        {
+            this.ten = ten;
+        }
+    }
+}
